2d0fda1acaf91da0ea970ebb629d3ff56828fccd
Zara Madureira <63427340+zaraandreia@users.noreply.github.com>
2022-02-17 13:14:37 +0000

    Update countmap.py

--- countmap.py
+++ countmap.py
@@ -4,7 +4,15 @@ import sys
 
 TAB_CHAR = '\t'
 
+#count the number of lines, words and characters
+n_lines = 0
+n_words = 0
+n_char = 0
 for line in sys.stdin:
+    n_lines+=1
     for token in line.strip().split(" "):
+        n_words +=1
+        n_char += len(token)
         if token: 
             print(token + TAB_CHAR + '1')
+
